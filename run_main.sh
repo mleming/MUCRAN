8de5f08dca435b0e622d8467c9ee99573a5f8e94
mleming <matthew.j.leming@gmail.com>
2022-05-12 22:31:09 -0400

    Added message to run_main

--- run_main.sh
+++ run_main.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python3 main.py --var_file=../../pandas/cache/all_vars_2.pkl -l=AlzStage -c SexDSC RepetitionTime
+# The three essential arguments for this are the label to classify by, the 
+# confounds to regress, and the var_file that stores the paths to all of the
+# data as well as the labels themselves.
+
+python3 main.py --var_file ../../pandas/cache/all_vars_2.pkl --l AlzStage -c SexDSC RepetitionTime
